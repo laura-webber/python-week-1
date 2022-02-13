7caa2f91763741c24dcc5a7190106fe665003767
Laura Webber <laura@hightouch.io>
2022-02-12 19:46:04 -0800

    add comments

--- main.py
+++ main.py
@@ -18,11 +18,11 @@
 # days_to_units(20)
 
 
-def days_to_units_message(num_of_days, message):
-    if num_of_days > 0:
-        print(f'Function with message output: {num_of_days} days are {to_seconds} {name_of_unit}, {message}')
-    else:
-        return "you entered a negative value"
+# def days_to_units_message(num_of_days, message):
+#     if num_of_days > 0:
+#         print(f'Function with message output: {num_of_days} days are {to_seconds} {name_of_unit}, {message}')
+#     else:
+#         return "you entered a negative value"
 
 
 # days_to_units_message(20, 'hi there')
@@ -31,22 +31,22 @@ def days_to_units_message(num_of_days, message):
 # print(greeting)
 
 
-def validate_and_execute(the_input):
-    try:
-        user_input_number = int(the_input)
-        if user_input_number > 0:
-            calculated_value = user_input_number * 2
-            print(calculated_value)
-        elif user_input_number == 0:
-            print("you entered a 0. please enter a valid number")
-        else:
-            print("you entered a negative number. no bueno")
-    except:
-        print("your input is not a valid number")
+# def validate_and_execute(the_input):
+#     try:
+#         user_input_number = int(the_input)
+#         if user_input_number > 0:
+#             calculated_value = user_input_number * 2
+#             print(calculated_value)
+#         elif user_input_number == 0:
+#             print("you entered a 0. please enter a valid number")
+#         else:
+#             print("you entered a negative number. no bueno")
+#     except:
+#         print("your input is not a valid number")
 
 
-user_input = ""
-while user_input != "exit":
-    user_input = input("please enter a comma separated list of numbers: ")
-    for num_of_days in user_input.split(", "):
-        validate_and_execute(user_input)
+# user_input = ""
+# while user_input != "exit":
+#     user_input = input("please enter a comma separated list of numbers: ")
+#     for num_of_days in user_input.split(", "):
+#         validate_and_execute(user_input)
