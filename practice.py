1a08f5c8aea3075c0b8d2d12fb4b446809533c92
Laura Webber <laura@hightouch.io>
2022-02-16 08:57:12 -0800

    Create practice.py

---
+++ practice.py
@@ -0,0 +1,17 @@
+print('hello')
+
+# age = 35
+# current_year = 2022
+
+# print(age)
+
+# year_of_birth = current_year - age
+
+# print(year_of_birth)
+
+
+def find_birth_year(age, current_year):
+    print(current_year - age)
+
+
+find_birth_year(35, 2022)
