2e60bd9967c937ee67233367927a344f76c3a34c
Laura Webber <laura@hightouch.io>
2022-02-22 17:07:15 -0800

    update

---
+++ exercises.py
@@ -0,0 +1,69 @@
+# 2 + 2
+print(2 + 2)
+
+# a = 2
+a = 2
+
+# print(a)
+print(a)
+
+# b = 2
+b = 2
+
+# print(b)
+print(b)
+
+# id(a)
+print(id(a))
+
+# id(b)
+print(id(b))
+
+# c = a + b
+c = a + b
+
+# print(c)
+print(c)
+
+# id(c)
+print(id(c))
+
+# b = a
+b = a
+
+# print(b)
+print(b)
+
+# id(a)
+print(id(a))
+
+# id(b)
+print(id(b))
+
+# calc = a+b*c
+calc = a + b + c
+
+# print(calc)
+print(calc)
+
+# word = ‘Hello World’
+# did you get an error? How would you fix this?
+# word = ‘Hello World’ - bad character error
+word = 'Hello World'
+print(word)
+
+# another_word = ', I Love Python'
+another_word = ', I Love Python'
+print(another_word)
+
+# STRETCH: Create a new variable with the phrase, Welcome to the Smith's farm.
+# did you get an error? How would you fix this?
+# Hint you can use both double and single quotes in Python if you are consistent.
+greeting = "Welcome to the Smith's farm"
+print(greeting)
+
+# print(word+another_word)
+print(word + another_word)
+
+# STRETCH: What’s another way to concatenate two strings in Python 3.7
+# Hint: Perform an internet search for "Python3 concatenate strings"
---
+++ terminology.py
@@ -0,0 +1,9 @@
+
+# TODO terms: markup languages, heterogeneous operating systems, runtime environment, compile, source code
+# TODO terms: machine language, high-level languages, hardware abstraction layer, memory blocks, garbage collection
+# TODO terms: abstraction, 3rd generation languages, binary, 4th gen languages, Python written in C language
+# TODO terms: refactoring, packages, Python3 vs Python2, object-oriented, functional programming
+# TODO terms: IDEs, PIP, Conda, Anaconda distribution
+# TODO terms: modules < packages < (sometimes) libraries < frameworks - collection of libraries/packages
+# TODO terms: repository, dependency, Spyder IDE, Pycharm IDE, shell, R
+# TODO terms: directory, console, terminal
